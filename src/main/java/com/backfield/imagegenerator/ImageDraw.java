3128530a2e7d8af3bd576e29fa8156ff6081caef
Joshua Backfield <suroot.chmod777@gmail.com>
2015-03-10 11:59:23 -0400

    Refactored the squareSize as a static method.

--- src/main/java/com/backfield/imagegenerator/ImageDraw.java
+++ src/main/java/com/backfield/imagegenerator/ImageDraw.java
@@ -7,7 +7,7 @@ public class ImageDraw {
 
     private BufferedImage image;
 
-    private int squareSize;
+    public static int squareSize;
     private int numberOfSquares;
 
     private int x = 0;
@@ -24,9 +24,9 @@ public class ImageDraw {
     private final int[] complimentPalette = {0xFFE7AA, 0xD4B66A, 0x806115, 0x553D00};
 
     public ImageDraw(int squareSize, int numberOfSquares) {
-        this.squareSize = squareSize;
+        ImageDraw.squareSize = squareSize;
         this.numberOfSquares = numberOfSquares;
-        this.image = new BufferedImage(this.squareSize * this.numberOfSquares, this.squareSize * this.numberOfSquares, BufferedImage.TYPE_INT_RGB);
+        this.image = new BufferedImage(ImageDraw.squareSize * this.numberOfSquares, ImageDraw.squareSize * this.numberOfSquares, BufferedImage.TYPE_INT_RGB);
     }
 
     public void setSeed(int hash) {
@@ -34,41 +34,41 @@ public class ImageDraw {
     }
 
     private void drawLowerLeftTriangle(BufferedImage image, int color, int x, int y) {
-        for (int squareX = 0; squareX < this.squareSize; squareX++) {
-            for (int squareY = squareX; squareY < this.squareSize; squareY++) {
-                image.setRGB(x * this.squareSize + squareX, y * this.squareSize + squareY, color);
+        for (int squareX = 0; squareX < ImageDraw.squareSize; squareX++) {
+            for (int squareY = squareX; squareY < ImageDraw.squareSize; squareY++) {
+                image.setRGB(x * ImageDraw.squareSize + squareX, y * ImageDraw.squareSize + squareY, color);
             }
         }
     }
 
     private void drawLowerRightTriangle(BufferedImage image, int color, int x, int y) {
-        for (int squareX = 0; squareX < this.squareSize; squareX++) {
-            for (int squareY = (this.squareSize - squareX); squareY < this.squareSize; squareY++) {
-                image.setRGB(x * this.squareSize + squareX, y * this.squareSize + squareY, color);
+        for (int squareX = 0; squareX < ImageDraw.squareSize; squareX++) {
+            for (int squareY = (ImageDraw.squareSize - squareX); squareY < ImageDraw.squareSize; squareY++) {
+                image.setRGB(x * ImageDraw.squareSize + squareX, y * ImageDraw.squareSize + squareY, color);
             }
         }
     }
 
     private void drawUpperLeftTriangle(BufferedImage image, int color, int x, int y) {
-        for (int squareX = 0; squareX < this.squareSize; squareX++) {
-            for (int squareY = 0; squareY < this.squareSize; squareY++) {
-                image.setRGB(x * this.squareSize + squareX, y * this.squareSize + squareY, color);
+        for (int squareX = 0; squareX < ImageDraw.squareSize; squareX++) {
+            for (int squareY = 0; squareY < ImageDraw.squareSize; squareY++) {
+                image.setRGB(x * ImageDraw.squareSize + squareX, y * ImageDraw.squareSize + squareY, color);
             }
         }
     }
 
     private void drawUpperRightTriangle(BufferedImage image, int color, int x, int y) {
-        for (int squareX = 0; squareX < this.squareSize; squareX++) {
+        for (int squareX = 0; squareX < ImageDraw.squareSize; squareX++) {
             for (int squareY = 0; squareY < squareX; squareY++) {
-                image.setRGB(x * this.squareSize + squareX, y * this.squareSize + squareY, color);
+                image.setRGB(x * ImageDraw.squareSize + squareX, y * ImageDraw.squareSize + squareY, color);
             }
         }
     }
 
     private void drawSquare(BufferedImage image, int color, int x, int y) {
-        for(int squareX = 0; squareX < this.squareSize; squareX++) {
-            for(int squareY = 0; squareY < this.squareSize; squareY++) {
-                image.setRGB(x * this.squareSize + squareX, y * this.squareSize + squareY, color);
+        for(int squareX = 0; squareX < ImageDraw.squareSize; squareX++) {
+            for(int squareY = 0; squareY < ImageDraw.squareSize; squareY++) {
+                image.setRGB(x * ImageDraw.squareSize + squareX, y * ImageDraw.squareSize + squareY, color);
             }
         }
     }
